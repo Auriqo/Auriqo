9605469fad3dfb8e85cd95bf3298f9931766b5c9
berruetaa <berruetx@gmail.com>
2026-08-12 02:39:03 -0300

    fix(letras): add missing ktor header import

--- letras/src/main/kotlin/com/auriqa/music/letras/LetrasCom.kt
+++ letras/src/main/kotlin/com/auriqa/music/letras/LetrasCom.kt
@@ -6,6 +6,7 @@ import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.get
+import io.ktor.client.request.header
 import io.ktor.client.request.parameter
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpHeaders
